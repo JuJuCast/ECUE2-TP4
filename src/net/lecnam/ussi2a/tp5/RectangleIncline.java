19ab983c698118d249c1a04c7741c74846d792f6
Julien Forestier <julien.forestier.auditeur@lecnam.net>
2026-09-22 12:25:07 +0200

    Exercice 7

--- src/net/lecnam/ussi2a/tp5/RectangleIncline.java
+++ src/net/lecnam/ussi2a/tp5/RectangleIncline.java
@@ -12,5 +12,17 @@ public class RectangleIncline extends Rectangle {
         super(longueur, largeur, x, y);
         this.angle = angle;
     }
+    public boolean contient(Point point) {
+        double a = Math.toRadians(this.angle);
+        double dx = point.x - this.point.x;
+        double dy = point.y - this.point.y;
+        double rotx =  dx * Math.cos(a) + dy * Math.sin(a);
+        double roty = -dx * Math.sin(a) + dy * Math.cos(a);
+        return 0 <= rotx && rotx <= this.longueur
+                && 0 <= roty && roty <= this.largeur;
+    }
+    public String toString() {
+        return "Rectangle [longueur=" + this.longueur + ", largeur=" + this.largeur + ", point=" + this.point + ", Angle=" + this.angle + "°]";
+    }
 
 }
